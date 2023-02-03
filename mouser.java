fc003eaca4b5e65e93590d8db4605c4efdd70290
itscold404 <81492047+itscold404@users.noreply.github.com>
2023-02-03 15:10:16 -0500

    Update mouser.java (#4)
    comments to mouser

--- mouser.java
+++ mouser.java
@@ -10,9 +10,16 @@ import java.awt.event.KeyEvent;
 import java.util.Random;
 
 public class mouser {
+	/**
+	This object is created to control mouse movements and clicks
+	 */
+
+	 //weights will be used to simulate a bellcurve/normal distriution
+	 //in findSafeCord(int min, int max)
 	double weight63 = 7.5; //weight for the 63% of area 
 	double weight31 = 3.5; //weight for 31% of area
 	int weight6 = 1; //weight for the outer 6% of area
+
 	Robot bot;
 	
 	public mouser(){
@@ -24,18 +31,22 @@ public class mouser {
 		}
 	}
 	
+	/**
+	*Moves mouse to a specified locaton and click at that location after a delay. 
+	*
+	*@param name the specified location where the mouse should move to and click
+	*@return null
+	*/
 	public void moveMouse(String name){ 
-		//takes name of the image that you want to click and moves mouse there
-		// make sure this calls safeCordFinder()
-		//coordinates of paint and actual coordinates used by program is a ~1.5:1 ratio
-		// (divide the coordinates obtained by paint by 1.5)
-		// the coordinates shown below are the already converted cords (no need to convert them)
 		int min_wait = 1500; //wait time before each mouse clicks to give time for windows to load
 		int max_wait = 2000;
 		int x = 0;
 		int y = 0;
 		
 		switch(name) {
+			//each case represents the imported png files. they are the coordinate of where
+			//that image should be located
+			//findSafeCord() finds a random corrdinate within the specified range of coordinates
 			case "plus": 
 //				max_x = 725; min_x = 716; min_y = 186; max_y = 195;
 				x = findSafeCord(713,738);
@@ -115,6 +126,7 @@ public class mouser {
 		}
 		
 		try{
+			//move mouse to a coordinate and click after a delay
 			bot.mouseMove(x,y);
 			Thread.sleep((int)(Math.random() * (max_wait - min_wait +1) + min_wait)); 
 			clickMouse();
@@ -124,9 +136,12 @@ public class mouser {
 		}
 	}
 	
+	/**
+	Return a boolean to show whether or not a certain state is reached
+	@param s the state to be reached or detected
+	@return tf boolean to show if the state was reached or detected
+	 */
 	public boolean check(String s) { 
-		//check pixle color at (565x369)376x246 and (572x371)381x247 (817x373)545x249
-		
 		boolean tf = false;
 		Rectangle capture;
 		BufferedImage check_screen;
@@ -139,11 +154,21 @@ public class mouser {
 		Color color3;
 		Color actual3;
 		try {
+			//create a screenshot of what is on the current screen
 			capture =  new Rectangle(Toolkit.getDefaultToolkit().getScreenSize()); 
 				
 			Image = bot.createScreenCapture(capture); 
 				    
 			switch(s){
+				//compare current screenshot to another screenshot with the desired state to
+				//see if the desired state is reached. if it is reached, change tf to true and return 
+				//it
+
+				//note: color1, color2, color3 represents the color of different pixels at thee
+				//different locations of the image of the desired state in microsoft paint
+				//actual1, actual2, actual3 represents the pixels from the current screenshot
+				//color1, color2, and color3 need to be converted(done by dividing x and y values by 1.5)
+				//to get the coordinates of their counterparts
 				case "battle ended":
 					raw_file = new File("C:\\Users\\linda\\eclipse-workspace\\SWProject\\replay.png");
 					check_screen = ImageIO.read(raw_file);
@@ -166,7 +191,6 @@ public class mouser {
 					raw_file = new File("C:\\Users\\linda\\eclipse-workspace\\SWProject\\notenoughenergy1.png");
 					check_screen = ImageIO.read(raw_file);
 					
-					// the pixels used from the paint file are: 754x398    801x401     906x407
 					color1 = new Color(check_screen.getRGB(754,398)); //paint coord1
 					actual1 = new Color(Image.getRGB(502, 265));//converted cord1
 					
@@ -195,14 +219,6 @@ public class mouser {
 					
 					if(color1.equals(actual1) && color2.equals(actual2) && color3.equals(actual3)) 
 						tf = true;
-//					System.out.println(tf);
-//					System.out.println(check_screen.getRGB(812,434));
-//					System.out.println(Image.getRGB(541, 289));
-//					System.out.println(check_screen.getRGB(1002,423));
-//					System.out.println(Image.getRGB(668, 282));
-//					System.out.println(check_screen.getRGB(1254,433));
-//					System.out.println(Image.getRGB(836,288));
-//					System.out.println(color3.equals(actual3));
 					break;
 				}
 			
@@ -213,11 +229,21 @@ public class mouser {
 		return tf;
 	}
 	
+	/**
+	 click something
+	 */
 	public void clickMouse() {
 		bot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
 	    bot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
 	}
 	
+	/**
+	Return a number between min and max based on a bell curve/normal distribution
+
+	@param min the lower bound of the random range of numbers
+	@param max the upper bound of the random range of numbers
+	@return an random integer to represent a coordinate value
+	 */
 	public int findSafeCord(int min, int max) {
 		
 		int eightx = (int)(Math.random() * 1000 * weight63);
