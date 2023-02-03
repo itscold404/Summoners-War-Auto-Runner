7b1d0094da0639b8f3e32c1d0e1bac3a56f872ab
itscold404 <81492047+itscold404@users.noreply.github.com>
2023-02-03 13:45:14 -0500

    New comments (#2)
    * Update README.md
    
    disclamer
    
    * Update runner.java
    
    new comments

--- runner.java
+++ runner.java
@@ -14,7 +14,6 @@ class gui extends JFrame{
 	private JButton raid;
 	private boolean cairos_bool;
 	private boolean raid_bool;
-//	public static int time = 0;    //the time the user chose to continue auto run
 	int mins = 0; //min user chosses
 	int hours = 0;//hours user chose
 	int sec = 0;
@@ -24,13 +23,15 @@ class gui extends JFrame{
 	private boolean see_screen;
 	mouser mouse = new mouser();
 	
-	/*
-	 * SET MINIMUM AND MAXIMUM WAIT TIME BEFORE BATTLE REPEATS HERE
-	 */
-	int min_wait = 1; //minimum wait time in seconds before finding finish screen
-	int max_wait = 69; //maximum wait time in seconds before finding finish screen
+	//SET MINIMUM AND MAXIMUM WAIT TIME BEFORE BATTLE REPEATS HERE
+	
+	int min_wait = 1; //minimum wait time in seconds before finding finish screen in seconds
+	int max_wait = 69; //maximum wait time in seconds before finding finish screen in seconds
 	
 	public gui(){
+		/**
+		Create start screen with options to select what game mode to automate
+		 */
 		super("helping you save your life");
 		
 		cairos = new JButton("Cairos Dungeon/Rift Beast");
@@ -41,16 +42,16 @@ class gui extends JFrame{
 		raid.setBounds(150,100,200,30);
 		add(raid);
 		
+		//upon press of cairos or raid, create a new frame to allow user to enter how much
+		//time they want to save
 		ActionListener timeRecorder = new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				//checking for which button is pressed and setting true to confirm what function user wants
 				if(((JButton)e.getSource()).equals(cairos)) {
 					cairos_bool = true;
-					System.out.println("cairos button pressed");
 				}
 				if(((JButton)e.getSource()).equals(raid)) {
 					raid_bool = true;
-					System.out.println("raid button pressed");
 				}
 				
 				frame2= new JFrame();
@@ -80,45 +81,25 @@ class gui extends JFrame{
 		cairos.addActionListener(timeRecorder);
 		raid.addActionListener(timeRecorder);
 		
-//		cairos.addActionListener(new ActionListener(){
-//			public void actionPerformed(ActionEvent e) {
-//				System.out.println("cairos button pressed");
-//				cairos_bool = true;
-//				frame2= new JFrame();
-//		        frame2.setSize(400, 200); 
-//		        
-//		        ok = new JButton("Confirm");
-//				ok.setBounds(180,120,100,30);
-//				frame2.add(ok);
-//				theHandler handler = new theHandler();
-//				ok.addActionListener(handler);
-//				
-//				time_prompt = new JLabel("how much time do you want to save? Enter time as \"hours:minutes\"");
-//				time_prompt.setBounds(20,20,400,40);
-//				frame2.add(time_prompt);
-//				
-//				choose_time = new JTextField();
-//				choose_time.setBounds(140,60,60,20);
-//				frame2.add(choose_time);
-//				
-//				frame2.getContentPane().setLayout(null);
-//		        frame2.setVisible(true);
-//		        frame2.setDefaultCloseOperation(EXIT_ON_CLOSE);
-//		        dispose();
-//			}
-//		});
 	}
 	
 	private class theHandler implements ActionListener 
 	{
+		/**
+		When the ok button is pressed to confirm the amount of time the user wants
+		to save, create a new frame to display the count down from the entered time. 
+		 */
 		public void actionPerformed(ActionEvent e) {
 			if(e.getSource() == ok)
 			{
+				//retrieve the time the user entered in the textbox
 				String raw_time = choose_time.getText();
 				int locH = raw_time.indexOf(":");
 				hours = Integer.parseInt(raw_time.substring(0, locH));
 				mins = Integer.parseInt(raw_time.substring(locH+1));
 				sec = 0;
+
+				//create the new frame and display that time in the format of "hours:minutes:seconds"
 				Timer timer = new Timer();
 				Timer mouse_delay = new Timer();
 				
@@ -130,7 +111,8 @@ class gui extends JFrame{
 				time_left_display = new JLabel(hours + " : " + mins + " : " + sec + " left on the timer");
 				frame3.add(time_left_display);
 				
-				
+				//while there is still time left on the timer, keep counting down. if there is no time left
+				//on the timer, display "battle complete"
 				timer.scheduleAtFixedRate(new TimerTask() {
 					public void run() {
 						Container parent = time_left_display.getParent();
@@ -189,8 +171,6 @@ class gui extends JFrame{
 						int rand = (int)(Math.random() * (max_wait - min_wait +1) + min_wait) * 1000;
 						try {
 							if(mouse.check("out of energy")) {
-								System.out.println("out of energy detected");
-//								mouse.moveMouse("plus");
 								mouse.moveMouse("shop");
 								// buy energy from shop
 								mouse.moveMouse("60 crystals");
